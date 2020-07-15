07f5f7d51fb81a630f2217d7906cdd4dc467bcc2
MargaritaSt <margarita.struts@me.com>
2020-07-15 00:53:28 +0000

    create composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,6 +3,7 @@
  //   let text = $(this).parent().children('.tweet-text').val();
   //  $(this).parent().children().children('.counter').html(text.length + 1);
 
+  
 $(document).ready(function() {
   $("#tweet-text").keyup(function(events)  {
     $("output.counter").text(140 - this.value.length);
