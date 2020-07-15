0766c5a290f9a1969fc612a11625431104c74250
MargaritaSt <margarita.struts@me.com>
2020-07-15 00:38:35 +0000

    Create compose-char-counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+
+//$(document).ready(function() {
+  //$("textarea").on('keydown', function(event) {
+ //   let text = $(this).parent().children('.tweet-text').val();
+  //  $(this).parent().children().children('.counter').html(text.length + 1);
+
+$(document).ready(function() {
+  $("#tweet-text").keyup(function(events)  {
+    $("output.counter").text(140 - this.value.length);
+    const log = this.value.length;
+    if (log > 140) {
+      $("output.counter").addClass("error");
+    } else {
+      $("output.counter").removeClass("error");
+    }
+  });
+});
+
+
