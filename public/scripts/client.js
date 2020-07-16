b583e58ba90891dd523cee6cc3669b391bc0a90b
Margarita Struts <margarita.struts@me.com>
2020-07-16 18:06:13 +0000

    create Get request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,74 +1,51 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-// Test / driver code (temporary). Eventually will get this from the server.
-// Fake data taken from initial-tweets.json
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
+const renderTweets = function(tweets) {
+  $('#tweets-container').empty();
+   tweets.forEach( (element) => {
+       let $tweet = createTweetElement(element);
+          $('#tweets-container').append($tweet);
+          //console.log($tweet); // to see what it looks like   
+   });
+}
 
- const renderTweets = function(tweets) {
-     tweets.forEach( (element) => {
-         let $tweet = createTweetElement(element);
-            $('#tweets-container').append($tweet);
-            //console.log($tweet); // to see what it looks like   
-     });
-  }
-
- const createTweetElement = function(tweet) {
-    let $tweet = `
-    <article class = "article">
-            <div class = "headers-footers-alignment">
-              <header class = "header-footer-format" style = "align-items: flex-start"> ${tweet.user.name} </header>
-              <header class = "header-hover" style = "align-items: flex-end;"> ${tweet.user.handle} </header>
-            </div>
-            <div >
-              <p style = "word-wrap: break-word; margin: 40px; padding-top: 20px;">${tweet.content.text}</p>
-            </div>
-            <div class = "headers-footers-alignment" style = "border-top: solid grey 3px;">
-              <footer class = "header-footer-format">${tweet.created_at}</footer>
-              <footer class = "header-footer-format">footer 2</footer>
+const createTweetElement = function(tweet) {
+  let $tweet = `
+  <article class = "article">
+          <div class = "headers-footers-alignment">
+            <header class = "header-footer-format" style = "align-items: flex-start"> ${tweet.user.name} </header>
+            <header class = "header-hover" style = "align-items: flex-end;"> ${tweet.user.handle} </header>
           </div>
-    </article>
-    `
-    return $tweet;
-  };
+          <div >
+            <p style = "word-wrap: break-word; margin: 40px; padding-top: 20px;">${tweet.content.text}</p>
+          </div>
+          <div class = "headers-footers-alignment" style = "border-top: solid grey 3px;">
+            <footer class = "header-footer-format">${tweet.created_at}</footer>
+            <footer class = "header-footer-format">footer 2</footer>
+        </div>
+  </article>
+  `
+  return $tweet;
+};
+
+//Get request
+const loadTweets =()=>{
+  $.ajax({url:'/tweets', method:'GET',}).then((response) => {
+    const $tweet = renderTweets (response); 
+  })
+};
+
+$(document).ready(function () {
+  loadTweets();
   
-  $(document).ready(function () {
-    const $tweet = renderTweets (data); 
-    $('#button').on('click', (evt) => {
-    //$('form').on('submit', (evt) => {  //alternative option. whatever event you use. The idea is to catch POST request and pass it to AJAX 
-        console.log($("#tweet-text").serialize());
-        evt.preventDefault();
-        $.post("/tweets", $('#tweet-text').serialize());
+  $('#button').on('click', (evt) => {
+  //$('form').on('submit', (evt) => {  //alternative option. whatever event you use. The idea is to catch POST request and pass it to AJAX 
+     // console.log($("#tweet-text").serialize());
+      evt.preventDefault();
+      $.post("/tweets", $('#tweet-text').serialize())
+      .then(() =>{
+        $('textarea').val('');
 
-    });     
- });
-
-
- 
-
+        loadTweets();
+      })
+  });     
+});
