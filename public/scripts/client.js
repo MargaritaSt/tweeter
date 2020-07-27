8eba3fc1ceeb7e9b3ee5f7bdaddabe75948f422d
Margarita Struts <margarita.struts@me.com>
2020-07-27 02:29:02 -0400

    Fixed: The bug with tweeting where a user cannot submit a tweet even the characters < 140, Fixed: The tweet box uses realtive times now, Fixed: empty If statement that was on the line 78. Tweet section was aligned with Compose Tweet. Code clean up was done as well

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,7 @@
 //Render tweets to index.html
 const renderTweets = function(tweets) {
   $('#tweets-container').empty();
-  let reversedTweets = tweets.reverse();
+  const reversedTweets = tweets.reverse();
    reversedTweets.forEach( (element) => {
       let $tweet = createTweetElement(element);
       $('#tweets-container').append($tweet); 
@@ -12,7 +12,6 @@ const renderErrors = function(error) {
   $('.error-text').text(error);
   $('.error-text').slideDown("slow");
   $('.error-text').css("border-color", "red");
-  
 };
 
 //Safe text. To prevent harmful text/scripts from being loaded to the server
@@ -22,28 +21,48 @@ const escape =  function(str) {
   return div.innerHTML;
 }
 
+//Calculating the number of days when Tweet was created.
+const getDate = function (date) {
+  const currentdate = new Date();
+  const tweetdate = (new Date(date));
+  let diffdate = currentdate.getTime() - tweetdate.getTime();
+  let tweetDateText;
+  diffdate = Math.round(diffdate/1000/60/60/24);
+  switch (diffdate){
+    case 0:
+      tweetDateText = "Today";
+      break;
+    case 1:
+      tweetDateText = diffdate + " day ago";
+      break;
+    default:
+      tweetDateText = diffdate + " days ago";
+  }
+  return tweetDateText;
+}
+
 //Creating a single tweet in HTML format. This function is used by renderTweets()
 const createTweetElement = function(tweet) {
-  let $tweet = `
+  const $tweet = `
   <article class = "article">
-          <div class = "headers-footers-alignment">
-             <header class = "header-footer-format" style = "align-items: flex-start;">
-             <div><img src = "${tweet.user.avatars}"> </div>
-             <div style = "margin-left: 10px; margin-top: 15px;">${tweet.user.name}</div>
-            </header>
-            <header class = "header-hover" style = "align-items: flex-end;"> ${tweet.user.handle} </header>
-          </div>
-          <div >
-            <p style = "word-wrap: break-word; margin: 40px; padding-top: 20px; font-size: 28px;">${escape(tweet.content.text)}</p>
-          </div>
-          <div class = "headers-footers-alignment" style = "border-top: solid grey 3px;">
-            <footer class = "header-footer-format">${(new Date(tweet.created_at)).toDateString()}</footer>
-            <footer class = "header-footer-format">
-              <i class = "fa fa-flag" style = "margin-right: 5px; color: #4056A1;"></i>   
-              <i class = "fa fa-retweet" style = "margin-right: 5px; color: #4056A1;"></i> 
-              <i class = "fa fa-heart" style = "color: #4056A1;"></i> 
-            </footer>
-        </div>
+    <div class = "headers-footers-alignment">
+      <header class = "header-footer-format" style = "align-items: flex-start;">
+        <div><img src = "${tweet.user.avatars}"> </div>
+        <div style = "margin-left: 10px; margin-top: 15px;">${tweet.user.name}</div>
+      </header>
+      <header class = "header-hover" style = "align-items: flex-end;"> ${tweet.user.handle} </header>
+    </div>
+    <div >
+      <p style = "word-wrap: break-word; margin: 40px; padding-top: 20px; font-size: 28px;">${escape(tweet.content.text)}</p>
+    </div>
+    <div class = "headers-footers-alignment" style = "border-top: solid grey 3px;">
+      <footer class = "header-footer-format">${getDate(tweet.created_at)}</footer>
+      <footer class = "header-footer-format">
+        <i class = "fa fa-flag" style = "margin-right: 5px; color: #4056A1;"></i>   
+        <i class = "fa fa-retweet" style = "margin-right: 5px; color: #4056A1;"></i> 
+        <i class = "fa fa-heart" style = "color: #4056A1;"></i> 
+      </footer>
+    </div>
   </article>
   `
   return $tweet;
@@ -59,7 +78,7 @@ const loadTweets =()=>{
 //Creating alert message when you click the "Tweet" button if text box is empty or has more than 140 chars
 const validation = ((data) => {
   let error = '';
-  let length = data.length - 5;
+  const length = data.length;
   if (length === 0) {
     error = 'Error: Field is empty. Type somthing!'; 
   } else if(length > 140) { 
@@ -73,16 +92,13 @@ $(document).ready(function () {
   $('.counter').val(140);
   $('#button').on('click', (evt) => {
   evt.preventDefault();
-
-  //error validation
-    if ($('.error-text').first().is( ":hidden" ) ) {
-    } else {
+    if (!($('.error-text').first().is(":hidden"))) {
       $('.error-text').hide();
     }
     $('.error-text').css("border-color", "transparent");
     $('.error-text').text('');
     $('.error-message').empty();
-    const validate = validation($('#tweet-text').serialize());
+    const validate = validation($('#tweet-text').val());
     if (validate === '') {
       let tweettext = $('#tweet-text') //data
       $.post("/tweets", tweettext.serialize())
